3d634f8aeb9b948e01e997a16d7879cff2ae348d
Michael Polianski <140472443+22011942@users.noreply.github.com>
2024-12-23 13:39:27 +1300

    Removing redundent code
    Cleaned up the wrokspace

--- src/mapGen.h
+++ src/mapGen.h
@@ -7,10 +7,4 @@ const int WORLD_HEIGHT = 10000;
 const int MAX_HEIGHT = WORLD_HEIGHT / BLOCK_SIZE;
 const int WORLD_WIDTH = 30000;
 
-//sf::Uint8 generateMap(sf::Uint8 *pixels);
-
-void generateMap(std::vector<std::vector<sf::RectangleShape>> *worldGrid);
-
-void generateChunk(int chunkX, int chunkY, std::map<std::pair<int, int>, std::vector<std::vector<sf::RectangleShape>>> *chunks);
-
 void generateChunkImprov(int chunkX, int chunkY, std::map<std::pair<int, int>, sf::VertexArray> *chunks);
